8999e9b40201a04884aa193727042989e14ae1d2
Anne Jones <anne1365@outlook.com>
2021-04-30 00:44:46 -0700

    Sync

--- sinatra_todos/todo.rb
+++ sinatra_todos/todo.rb
@@ -38,9 +38,36 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
-# Create a new list
-post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  redirect "/lists"
+# Return an error message if the list name is invalid, otherwise return nil
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters long."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
 end
 
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, id: session[:lists].size,  todos: []}
+    session[:success] = "The list has been created!"
+    redirect "/lists"
+  end
+end
+
+get "/lists/:id" do
+  id = params[:id].to_i
+  @lists = session[:lists]
+  @list = []
+  session[:lists].each do |hash|
+    @list << hash if hash[:id] == id
+  end
+  erb :list, layout: :layout
+end
