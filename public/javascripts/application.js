28106a73f36cc13a54555f2ca9e89cbd9a0e2e64
Anne Jones <anne1365@outlook.com>
2021-05-10 23:53:21 -0700

    Sync

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,13 @@
-console.log("This is a test.");
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+
+});
