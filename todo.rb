d7b3750600521a34c058d3bbda452ae2c9aceee6
Anne Jones <anne1365@outlook.com>
2021-07-29 16:53:05 -0700

    Setting up a database connection

--- todo.rb
+++ todo.rb
@@ -3,6 +3,8 @@ require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
+require_relative "session_persistence"
+
 ## CONFIG----------------------------------------------------------------------
 configure do
   enable :sessions
@@ -44,66 +46,6 @@ helpers do
 
     incomplete_todos.each(&block)
     complete_todos.each(&block)
-  end
-end
-
-## SESSION PERSISTENCE CLASS---------------------------------------------------
-class SessionPersistence
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find { |l| l[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list_name(id, new_list_name)
-    list = find_list(id)
-    list[:name] = new_list_name
-  end
-
-  def create_new_todo(list_id, todo_text)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << { id: id, name: todo_text, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-
-  end
-
-  def update_todo_completion_status(list_id, todo_id, new_completion_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |t| t[:id] == todo_id }
-    todo[:completed] = new_completion_status
-  end
-
-  def mark_all_todos_complete(list_id)
-    list = find_list(list_id)
-    list[:todos].each { |todo| todo[:completed] = true }
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |el| el[:id] }.max || 0
-    max + 1
   end
 end
 
